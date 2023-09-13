f5b9031a52b04c02d03f0fe8165124cb66d03844
Andreas Himmler <13476558+Andy0n@users.noreply.github.com>
2023-09-13 02:18:06 +0200

    logging and restructuring

--- src/stowng/__main__.py
+++ src/stowng/__main__.py
@@ -1,4 +1,27 @@
-from . import stowng
+import logging
+
+from .parser import process_options
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def main():
+    """
+    The main function.
+
+    .. todo:: testing
+    .. todo:: documentation
+    .. todo:: logging levels correct?
+    """
+    options, delete, stow = process_options()
+
+    logging.getLogger().setLevel(10*options['verbosity'])
+
+    log.debug(f'Options:')
+    for key, value in options.items():
+        log.debug(f'    {key}: {value}')
+
+    print('Hello, StowNG!')
 
 if __name__ == '__main__':
-    stowng.main()
+    main()
--- src/stowng/stowng.py
+++ src/stowng/stowng.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-from .parser import process_options
-
-verbosity = 0
-
-def main():
-    process_options()
-
-    print('Hello, StowNG!')
-
